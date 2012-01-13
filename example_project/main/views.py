6e3f50052567ddbd2365d18918d1aff7efd9f1d0
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-01-13 15:12:09 -0800

    Updated main.views to use SendGridEmailMessage

--- example_project/main/views.py
+++ example_project/main/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 
 from main.forms import EmailForm
 from sendgrid.utils import send_email_with_sendgrid
+from sendgrid.message import SendGridEmailMessage
 
 def send_simple_email(request):
 	if request.method == 'POST': # If the form has been submitted...
@@ -11,11 +12,24 @@ def send_simple_email(request):
 		if form.is_valid(): # All validation rules pass
 			# Process the data in form.cleaned_data
 			# ...
-			send_email_with_sendgrid(
-				subject=request.POST["subject"],
-				message=request.POST["message"],
-				from_email=request.POST["sender"],
-				recipient_list=[request.POST["to"]])
+			subject=request.POST["subject"]
+			message=request.POST["message"]
+			from_email=request.POST["sender"]
+			recipient_list=[request.POST["to"]]
+			
+			# send_email_with_sendgrid(
+			# 	subject=request.POST["subject"],
+			# 	message=request.POST["message"],
+			# 	from_email=request.POST["sender"],
+			# 	recipient_list=[request.POST["to"]])
+				
+			email = SendGridEmailMessage(
+				subject,
+				message,
+				from_email,
+				['ryan@mindsnacks.com'],
+			)
+			email.send()
 			return HttpResponseRedirect('/') # Redirect after POST
 	else:
 		form = EmailForm() # An unbound form
