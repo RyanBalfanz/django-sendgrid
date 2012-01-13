511938ae5d29a89ace85ff0540d056d4a999e36e
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-01-13 15:20:15 -0800

    Removed defaults

--- example_project/sendgrid/backends.py
+++ example_project/sendgrid/backends.py
@@ -1,10 +1,6 @@
 from django.conf import settings
 from django.core.mail.backends.smtp import EmailBackend
 
-SENDGIRD_USE_DEFAULTS = getattr(settings, "SENDGIRD_USE_DEFAULTS", False)
-SENDGRID_DEFAULT_EMAIL_HOST = "smtp.sendgrid.net"
-SENDGRID_DEFAULT_EMAIL_PORT = 587
-
 SENDGRID_EMAIL_HOST = getattr(settings, "SENDGRID_EMAIL_HOST", None)
 SENDGRID_EMAIL_PORT = getattr(settings, "SENDGRID_EMAIL_PORT", None)
 SENDGRID_EMAIL_USERNAME = getattr(settings, "SENDGRID_EMAIL_USERNAME", None)
@@ -16,12 +12,6 @@ class SendGridEmailBackend(EmailBackend):
 	A wrapper that manages the SendGrid SMTP network connection.
 	"""
 	def __init__(self, host=None, port=None, username=None, password=None, use_tls=None, fail_silently=False, **kwargs):
-		# if not SENDGRID_EMAIL_HOST and SENDGIRD_USE_DEFAULTS:
-		# 	SENDGRID_EMAIL_HOST = SENDGRID_DEFAULT_EMAIL_HOST
-			
-		# if not SENDGRID_EMAIL_PORT and SENDGIRD_USE_DEFAULTS:
-		# 	SENDGRID_EMAIL_PORT = SENDGRID_DEFAULT_EMAIL_PORT
-			
 		super(SendGridEmailBackend, self).__init__(
 			host=SENDGRID_EMAIL_HOST,
 			port=SENDGRID_EMAIL_PORT,
