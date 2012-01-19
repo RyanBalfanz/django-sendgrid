c72a5c0e82c7a70101b147635aa679991aa646a8
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-01-18 22:01:22 -0800

    Added logger

--- example_project/main/models.py
+++ example_project/main/models.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth.models import User
 from django.core.mail import get_connection
 from django.core.mail import EmailMessage
@@ -5,6 +7,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=User)
 def send_new_user_email(sender, instance, created, raw, using, **kwargs):
