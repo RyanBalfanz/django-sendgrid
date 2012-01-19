9a97a9f80db908d6dc34515876c8f97eb6c93dfc
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-01-18 21:41:18 -0800

    Added sendgrid.admin

---
+++ sendgrid/admin.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+# django-sendgrid
+from models import EmailTemplate
+
+
+admin.site.register(EmailTemplate)
