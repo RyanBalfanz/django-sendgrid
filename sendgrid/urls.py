d5bdda853b35c1944717c7f7d29daf19d5c3d5e9
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-03-22 03:17:39 -0700

    Removed dead code

--- sendgrid/urls.py
+++ sendgrid/urls.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 
 from django.conf.urls.defaults import patterns, include, url
 
-# from main.views import send_simple_email
-
 from .views import listener
 
 
