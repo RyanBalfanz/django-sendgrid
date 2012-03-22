7b10d08c19b9a0cf57fc4933098342bbeace3ea0
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-03-22 03:14:21 -0700

    Reverted STATIC_ROOT

--- example_project/settings.py
+++ example_project/settings.py
@@ -57,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/znaflab/webapps/djsendgrid/lib/python2.7/django/contrib/admin/media/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
