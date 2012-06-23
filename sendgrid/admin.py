4cedc16e400fbb9955b9ffe1787fbd01d3fc6614
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-06-23 03:53:54 -0700

    Removed EmailMessage model

--- sendgrid/admin.py
+++ sendgrid/admin.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import
 
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
 
 from .models import SendGridEvent
-from .models import SendGridEmailMessage
 
 
 class SendGridEventInline(admin.TabularInline):
@@ -13,33 +11,6 @@ class SendGridEventInline(admin.TabularInline):
 	extra = 0
 	verbose_name = "Event"
 	verbose_name_plural = "Events"
-
-
-class SendGridEmailMessageAdmin(admin.ModelAdmin):
-	list_display = ("message_id", "creation_time", "last_modified_time", "status", "event_count")
-	# list_filter = ("status",)
-	search_fields = ("message_id",)
-	list_select_related = True
-	
-	inlines = [SendGridEventInline]
-	
-	fieldsets = (
-		(None, {
-			"fields": ("message_id",)
-		}),
-		(_("Text Body"), {
-			"fields": ("text_body",),
-			"classes": ("collapse", "closed")
-		}),
-		(_("HTML Body"), {
-			"fields": ("html_body",),
-			"classes": ("collapse", "closed")
-		}),
-		# (_("Advanced"), {
-		# 	"fields": ("headers", "attachments"),
-		# 	"classes": ("collapse", "closed")
-		# })
-	)
 
 
 admin.site.register(SendGridEmailMessage, SendGridEmailMessageAdmin)
--- sendgrid/models.py
+++ sendgrid/models.py
@@ -24,17 +24,6 @@ if SENDGRID_USER_MIXIN_ENABLED:
 
 logger = logging.getLogger(__name__)
 
-@receiver(sendgrid_email_sent)
-def create_sendgrid_email_message(sender, **kwargs):
-	logger.debug("E-mail sent with SendGrid")
-	
-	msg = kwargs["message"]
-	resp = kwargs["response"]
-
-	emsg = SendGridEmailMessage(
-		message_id=msg.message_id,
-	)
-	emsg.save()
 
 @receiver(sendgrid_event_processed)
 def handle_sendgrid_event(sender, **kwargs):
@@ -62,54 +51,6 @@ def handle_sendgrid_event(sender, **kwargs):
 	)
 
 
-class SendGridEmailMessage(models.Model):
-	"""(SendGridEmailMessage description)"""
-	message_id = models.CharField(blank=True, null=True, max_length=36) # Version 4 UUID
-	creation_time = models.DateTimeField(blank=True, default=datetime.datetime.now)
-
-	from_email = models.EmailField()
-	recipient_email = models.EmailField()
-	# recipient_email_list = JSONField(blank=True, null=True)
-	# cc_email_list = JSONField(blank=True, null=True)
-	# bcc_email_list = JSONField(blank=True, null=True)
-	reply_to_email = models.EmailField(blank=True, null=True)
-
-	subject = models.CharField("Subject", max_length=SENDGRID_EMAIL_MESSAGE_MAX_SUBJECT_LENGTH)
-	text_body = models.TextField("Text Body", blank=True, null=True)
-	html_body = models.TextField("HTML Body", blank=True, null=True)
-
-	last_modified_time = models.DateTimeField(blank=True, default=datetime.datetime.now)
-
-	# events = models.ManyToManyField(Event)
-
-	class Meta:
-		verbose_name = _("E-mail Message")
-		verbose_name_plural = _("E-mail Messages")
-
-	def __unicode__(self):
-		return u"{}".format(self.message_id)
-
-	def save(self, *args, **kwargs):
-		self.last_modified_time = datetime.datetime.now()
-		super(SendGridEmailMessage, self).save(*args, **kwargs)
-
-	def get_event_count(self):
-		return self.events.count()
-	event_count = property(get_event_count)
-
-	def get_latest_event(self):
-		latestEvent = None
-		if self.events.exists():
-			self.events.all()[0]
-
-		return latestEvent
-	latest_event = property(get_latest_event)
-
-	def get_latest_event_type(self):
-		lastestEvent = self.get_latest_event()
-		return lastestEvent.get_type_display() if lastestEvent else "No Events"
-	status = property(get_latest_event_type)
-
 class SendGridEvent(models.Model):
 	SENDGRID_EVENT_UNKNOWN_TYPE = EVENT_TYPES_MAP["UNKNOWN"]
 	SENDGRID_EVENT_DEFERRED_TYPE = EVENT_TYPES_MAP["DEFERRED"]
@@ -133,7 +74,6 @@ class SendGridEvent(models.Model):
 		(SENDGRID_EVENT_SPAM_REPORT_TYPE, "Spam Report"),
 		(SENDGRID_EVENT_UNSUBSCRIBE_TYPE, "Unsubscribe"),
 	)
-	email_message = models.ForeignKey(SendGridEmailMessage, related_name="events", verbose_name="Email Message")
 	creation_time = models.DateTimeField(blank=True, default=datetime.datetime.now)
 	
 	type = models.IntegerField(blank=True, null=True, choices=SENDGRID_EVENT_TYPES, default=SENDGRID_EVENT_UNKNOWN_TYPE)
