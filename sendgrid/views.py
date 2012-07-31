19b04ae442cbb7c24c80f802df346b689f13e3f7
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-07-30 17:31:40 -0700

    Removed a bunch of code, still needs some cleanup

--- sendgrid/views.py
+++ sendgrid/views.py
@@ -22,81 +22,6 @@ OPTIONAL_KEYS = ("category")
 
 logger = logging.getLogger(__name__)
 
-
-class SendGridEventValueError(ValueError):
-	pass
-
-
-def handle_single_json_event(e):
-	"""
-	Handles a single JSON event -- i.e. a single line in batched events.
-
-	Example JSON event ::
-
-		{"email":"foo@bar.com","timestamp":1322000095,"unique_arg":"my unique arg","event":"delivered"}
-
-	"""
-	if isinstance(e, basestring):
-		e = simplejson.loads(e)
-
-	email = e.get("email", None)
-	event = e.get("event", None)
-	category = e.get("category", None)
-	keyIsUniqueArg = lambda k: k not in REQUIRED_KEYS and k not in OPTIONAL_KEYS
-	uniqueArgs = dict(((k, v) for (k, v) in e.iteritems() if keyIsUniqueArg(k)))
-
-	if not email or not event:
-		issues = []
-		if not email:
-			issues.append("email")
-		if not event:
-			issues.append("event")
-
-		missingItems = ", ".join(issues)
-		exceptionMessage = "{missing} does not exist".format(missing=missingItems)
-		raise SendGridEventValueError(exceptionMessage)
-
-	result = {
-		"email": email,
-		"event": event,
-		"category": category,
-		"unique_args": uniqueArgs
-	}
-	return result
-
-def convert_single_event_request_to_json_event(request):
-	"""
-	Returns a JSON-style event.
-	"""
-	postData = request.POST
-	logger.debug("Post data: {0}".format(postData))
-
-	# Parameters that are always passed with each event
-	email = postData.get("email", None)
-	event = postData.get("event", None)
-	
-	# Optional parameters and categories
-	category = postData.get("category", None)
-	
-	# Custon parameters, unique arguments
-	uniqueArgs = {}
-	for k, v in postData.iteritems():
-		if k in REQUIRED_KEYS or k in OPTIONAL_KEYS:
-			continue
-		else:
-			uniqueArgs[k] = v
-
-	logger.debug("Unique Args: {0}".format(uniqueArgs))
-
-	eventDict = {
-		"email": email,
-		"event": event,
-	}
-	eventDict.update(uniqueArgs)
-	jsonEvent = simplejson.dumps(eventDict)
-
-	return jsonEvent
-
 def handle_single_event_request(request):
 	"""
 	Handles single event POST requests.
@@ -120,23 +45,12 @@ def handle_single_event_request(request):
 
 		response = HttpResponse()
 		response.write(eventObj)
-		return response
 	else:
-		logger.debug("Expected 'message_id' was not found in event data")
-		return HttpResponseBadRequest()
-
-	jsonEvent = convert_single_event_request_to_json_event(request)
-	jsonEvent = simplejson.loads(jsonEvent)
-
-	try:
-		result = handle_single_json_event(jsonEvent)
-	except SendGridEventValueError as e:
+		msg = "Expected 'message_id' was not found in event data"
+		logger.debug(msg)
 		response = HttpResponseBadRequest()
-		response.write("EXCEPTION: {e}\n".format(e=e))
-	else:
-		sendgrid_event_processed.send(sender=None, detail=result)
-		response = HttpResponse()
-	
+		response.write(msg)
+
 	return response
 
 def handle_batched_events_request(request):
