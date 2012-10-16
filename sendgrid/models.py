37d935aaa42b413d062a8f4a28680aea5e13c2fc
Tom Hoddes <tom@mindsnacks.com>
2012-10-15 17:59:33 -0700

    unique constraint added to creation_time for Event

--- sendgrid/models.py
+++ sendgrid/models.py
@@ -388,7 +388,7 @@ class Event(models.Model):
 	email_message = models.ForeignKey(EmailMessage)
 	email = models.EmailField()
 	event_type = models.ForeignKey(EventType)
-	creation_time = models.DateTimeField()
+	creation_time = models.DateTimeField(unique=True)
 	last_modified_time = models.DateTimeField(auto_now=True)
 
 	class Meta:
