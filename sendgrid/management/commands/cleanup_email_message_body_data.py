b57a29e46fbe7543c78e7cea2da10b9054f19e51
Ryan Balfanz <ryan@ryanbalfanz.net>
2012-11-21 13:30:05 -0800

    #28; Added working management command to purge body data

---
+++ sendgrid/management/commands/cleanup_email_message_body_data.py
@@ -0,0 +1,108 @@
+import datetime
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.timezone import now as now_utc
+
+from sendgrid.models import EmailMessage
+from sendgrid.models import EmailMessageBodyData
+
+
+ONE_DAY = datetime.timedelta(days=1)
+ONE_WEEK = datetime.timedelta(weeks=1)
+
+def filter_email_messages_created_before(start):
+	return EmailMessage.objects.filter(
+		creation_time__lt=start,
+	)
+
+# def filter_email_messages_in_range(start, end):
+# 	return EmailMessage.objects.filter(
+# 		creation_time__gte=start,
+# 		creation_time__lte=end
+# 	)
+
+# def filter_email_message_body_data_objects_created_in_range(start, end):
+# 	return EmailMessageBodyData.filter(
+# 		creation_time__gte=start,
+# 		creation_time__lte=end)
+
+# def filter_email_message_body_data_objects_created_since(start):
+# 	filteredObjects = EmailMessageBodyData.filter(**filters)
+
+# 	return baseObjects.filter(creation_time__gte=start)
+
+# def filter_email_message_body_data_objects(**filters):
+# 	filteredObjects = EmailMessageBodyData.filter(**filters)
+
+
+class Command(BaseCommand):
+	help = "Purges old EmailMessageBodyData objects"
+	option_list = BaseCommand.option_list + (
+	make_option('--as-date',
+		default=False,
+		action="store_true",
+		help="Sets the number of days"
+		),
+	make_option('--fake',
+		default=False,
+		action="store_true",
+		help="Fakes the operation"
+		),
+	make_option('--days',
+		default=0,
+		type="int",
+		help="Sets the number of days"
+		),
+	make_option('--weeks',
+		default=0,
+		type="int",
+		help="Sets the number of weeks"
+		),
+	)
+
+	def handle(self, *args, **kwargs):
+		nowUTC = now_utc()
+		todayUTC = nowUTC.date()
+
+		if all([kwargs["days"], kwargs["weeks"]]):
+			raise CommandError("Ambiguous arguments given")
+
+		if "days" in kwargs:
+			delta = kwargs["days"]*ONE_DAY
+			deltaType = "days"
+		elif "weeks" in kwargs:
+			delta = kwargs["weeks"]*ONE_WEEK
+			deltaType = "weeks"
+		else:
+			raise CommandError
+
+		purgeDate = nowUTC - delta
+		if kwargs["as_date"] == True:
+			purgeDate = purgeDate.date()
+
+		emailMessages = filter_email_messages_created_before(purgeDate)
+		numEmailMessages = emailMessages.count()
+
+		prompt = "Delete {m} objects created before {date} ({n} {t} ago)?"
+		print prompt.format(
+			m=numEmailMessages,
+			date=purgeDate,
+			n=kwargs["days"] if deltaType == "days" else kwargs["weeks"],
+			t=deltaType,
+		)
+
+		fake = kwargs["fake"]
+		for emailMessage in emailMessages:
+			try:
+				bodyDataObject = emailMessage.body
+			except EmailMessageBodyData.DoesNotExist:
+				print "EmailMessage {em} has no EmailMessageBodyData".format(em=emailMessage)
+				continue
+
+			if not fake:
+				bodyDataObject.delete()
+			print "{prefix}Deleted body data for {em}".format(
+				em=emailMessage,
+				prefix="(FAKE) " if fake else ""
+			)
