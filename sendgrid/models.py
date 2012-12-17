55f63b29ee10419d2b8650c6260a7a5026756315
Tom Hoddes <tom@mindsnacks.com>
2012-12-17 10:12:41 -0800

    create changed to get_or_create for EmailMessage in from_event

--- sendgrid/models.py
+++ sendgrid/models.py
@@ -221,7 +221,7 @@ class EmailMessage(models.Model):
 			if len(categories) > 0:
 				emailMessageSpec["category"] = categories[0]
 				
-			emailMessage = EmailMessage.objects.create(**emailMessageSpec)
+			emailMessage = EmailMessage.objects.get_or_create(**emailMessageSpec)
 			
 			for category in categories:
 				categoryObj,created = Category.objects.get_or_create(name=category)
